15cbd59f1ef1a6f826101e4765cfbc8ffe6f4731
amrrkhaled <amr.khaledsaada@gmail.com>
2025-08-11 14:50:59 +0300

    Implemented di to provide db and dao's

--- app/src/main/java/com/example/nectar/di/DatabaseModule.kt
+++ app/src/main/java/com/example/nectar/di/DatabaseModule.kt
@@ -1,2 +1,55 @@
 package com.example.nectar.di
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+
+import com.example.nectar.data.local.AppDatabase
+import com.example.nectar.data.local.dao.CartItemDao
+import com.example.nectar.data.local.dao.CategoryDao
+import com.example.nectar.data.local.dao.ProductDao
+import com.example.nectar.data.local.entity.CategoryEntity
+import com.example.nectar.data.local.entity.ProductEntity
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext appContext: Context,
+        // Inject your DAOs here or a CoroutineScope if needed
+    ): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "nectar_db"
+        )
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideProductDao(db: AppDatabase): ProductDao = db.productDao()
+
+    @Provides
+    @Singleton
+    fun provideCartItemDao(db: AppDatabase): CartItemDao = db.cartItemDao()
+
+    @Provides
+    @Singleton
+    fun provideCategoryDao(db: AppDatabase): CategoryDao = db.categoryDao()
+}
